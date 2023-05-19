6d431c383179dd4a6dbf7980522e9508f1fa1d87
Graham Rigby <graham.rigby@sniip.com>
2023-05-19 10:59:49 +1000

    public access

--- StripeCardScan/StripeCardScan/Source/CardScan/CardUtils/CardType.swift
+++ StripeCardScan/StripeCardScan/Source/CardScan/CardUtils/CardType.swift
@@ -13,7 +13,7 @@ public enum CardType: Int {
     case PREPAID
     case UNKNOWN
 
-    func toString() -> String {
+    public func toString() -> String {
         switch self {
         case .CREDIT: return "Credit"
         case .DEBIT: return "Debit"
@@ -22,7 +22,7 @@ public enum CardType: Int {
         }
     }
 
-    static func fromString(_ str: String) -> CardType {
+    public static func fromString(_ str: String) -> CardType {
         switch str.lowercased() {
         case "credit": return .CREDIT
         case "debit": return .DEBIT
